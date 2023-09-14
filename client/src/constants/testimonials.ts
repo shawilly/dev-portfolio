e90954bcf92363e41e71c9f46fe999c75314de2c
Shane Williams <shanebarrywilliams@gmail.com>
2023-09-14 13:19:58 +0100

    add testimony

--- client/src/constants/testimonials.ts
+++ client/src/constants/testimonials.ts
@@ -10,4 +10,13 @@ export const testimonials: ITestimonial[] = [
     image:
       "https://media.licdn.com/dms/image/D5635AQH6wvKFudTokA/profile-framedphoto-shrink_400_400/0/1694490983173?e=1695207600&v=beta&t=DJ01IqdVQsSMvmCZF8Ru6Uqi9Texh3ehEIUynhf60OA",
   },
+  {
+    testimonial:
+      "He constantly tries to learn more and is not afraid to ask questions. For every decision we made as a team, Shane would be an advocate for our users showing empathy and a deep understanding of the user's experience.",
+    name: "Francesca Ho",
+    designation: "Software Developer",
+    company: "BioRender",
+    image:
+      "https://media.licdn.com/dms/image/D4E03AQE2feJw9yiexQ/profile-displayphoto-shrink_400_400/0/1679290808754?e=1700092800&v=beta&t=271IXripvNgZtQ4toEBUKqu-YQZ41TrecEo75sJAszg",
+  },
 ];
