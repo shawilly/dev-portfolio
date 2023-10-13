3f97f3503c8fd7fafdb656658a60fb66357c26f1
Shane Williams <shanebarrywilliams@gmail.com>
2023-10-13 13:52:28 +0100

    add remove files from build

---
+++ client/src/utils/nextImages.ts
@@ -0,0 +1,12 @@
+export function getNextElements<T>(current: T[], array: T[], amount = 5): T[] {
+  if (array.length < amount) {
+    throw new Error("The array must have at least 5 elements.");
+  }
+  const start = array.findIndex((el) => el === current[current.length - 1]) + 1;
+  const length = array.length;
+  const indexes: number[] = [];
+  for (let i = 0; i < amount; i++) {
+    indexes.push((start + i) % length);
+  }
+  return indexes.map((index) => array[index]) as T[];
+}
